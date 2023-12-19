62aa08b2dbd3de27a5f38a7e77b0ef9135d9abb0
surgeryxx <kdmggo@gmail.com>
2023-12-19 14:32:39 +0900

    feat: 프로그래머스 - 소수 구하기 문제(통과)

--- ProgrammersTest.java
+++ ProgrammersTest.java
@@ -9,7 +9,7 @@ public class ProgrammersTest {
 
     @Test
     @DisplayName("프로그래머스 > 2017 팁스타운 > 예상 대진표")
-    void solution() {
+    void solution1() {
 
         int n = 256, a = 4, b = 6, answer = 0;
 
@@ -32,6 +32,33 @@ public class ProgrammersTest {
         }
 
         int expect = 3;
+        assertEquals(expect, answer);
+    }
+
+    @Test
+    @DisplayName("프로그래머스 > Summer/Winter Coding(~2018) > 소수 만들기")
+    void solution2() {
+        int[] nums = new int[]{1, 2, 7, 4, 6};
+        int answer = 0;
+
+        for (int i = 0; i <= (nums.length - 3); i++) {
+            for (int x = (i + 1); x <= (nums.length - 2); x++) {
+                for (int y = (x + 1); y <= (nums.length - 1); y++) {
+                    // 배열 인덱스를 통해 숫자의 합을 구한다.
+                    int calc = nums[i] + nums[x] + nums[y];
+
+                    // 소수 알고리즘으로 소수 여부를 확인하고, 소수가 아닐시 isPrime 값을 false로 재정의한다.
+                    boolean isPrime = true;
+                    for (int z = 3; z < calc; z++) {
+                        if (calc % z == 0) isPrime = false;
+                    }
+
+                    if (isPrime) answer++;
+                }
+            }
+        }
+
+        int expect = 4;
         assertEquals(expect, answer);
     }
 }
