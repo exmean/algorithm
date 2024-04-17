d9cbc0eac27d51f1b6daf822ce5e6d6e5d812e1a
DM <152713828+exmean@users.noreply.github.com>
2024-04-17 13:21:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 132 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -12,29 +12,23 @@ public class Main {
         br.close();
 
         int value = 0;
-        if ( st.countTokens() == 1 ) {
-            value = Arrays.stream(st.nextToken().split("\\+"))
-                .mapToInt(Integer::parseInt)
-                .sum();
 
-        } else {
-            int[] arr = new int[st.countTokens()];
-            for ( int i = 0; i < arr.length; i++ ) {
-                String current = st.nextToken();
-                if ( !current.contains("+") ) {
-                    arr[i] = Integer.parseInt(current);
-                    continue;
-                }
-
-                arr[i] = Arrays.stream(current.split("\\+"))
-                    .mapToInt(Integer::parseInt)
-                    .sum();
+        int[] arr = new int[st.countTokens()];
+        for ( int i = 0; i < arr.length; i++ ) {
+            String current = st.nextToken();
+            if ( !current.contains("+") ) {
+                arr[i] = Integer.parseInt(current);
+                continue;
             }
 
-            value = Arrays.stream(arr)
-                .reduce((a, b) -> a - b)
-                .getAsInt();
+            arr[i] = Arrays.stream(current.split("\\+"))
+                .mapToInt(Integer::parseInt)
+                .sum();
         }
+
+        value = Arrays.stream(arr)
+            .reduce((a, b) -> a - b)
+            .getAsInt();
 
         System.out.println(value);
     }
